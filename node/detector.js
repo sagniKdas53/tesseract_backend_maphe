1daac092c3b5a063c2acc07bc3b8a460d017aacd
sagnik <sagnikdas53@live.com>
2020-11-29 16:34:24 +0530

    the detector runs but wrongly

--- node/detector.js
+++ node/detector.js
@@ -6,32 +6,14 @@ window.onload = function () {
     let reff = document.getElementById('ref')
     let inputElement = document.getElementById('fileInput');
     console.log(reff.src);
-    console.log(top_right.src);
+    console.log("This is the one being " + top_right.src);
     console.log(top_left.src);
     console.log(imgElement.src);
-    //right
-    let canvastopr = document.getElementById('cTopr');
-    var ctr = canvastopr.getContext('2d');
-    ctr.width = 72;
-    ctr.height = 66;
-    ctr.drawImage(top_right,0,0);
-    //left
-    let canvastopl = document.getElementById('cTopl');
-    var ctl = canvastopl.getContext('2d');
-    ctl.width = 75;
-    ctl.height = 78;
-    ctl.drawImage(top_left,0,0);
-    //centered
-    let canvascc = document.getElementById('cref');
-    var ctc = canvascc.getContext('2d');
-    console.log(reff.src)
-    ctc.width = 99;
-    ctc.height = 339;
-    ctc.drawImage(reff,0,0);
     //made the loader will reimplement once the detector works
     //this is used by setting the visibilty once tesseract starts working and stopping
     //stopping once it's done
-    var loader = document.getElementById("loader");
+    //var loader = document.getElementById("loader");
+    
     inputElement.addEventListener('change', (e) => {
         imgElement.src = URL.createObjectURL(e.target.files[0]);
     }, false);
@@ -39,17 +21,34 @@ window.onload = function () {
         //printing to see if the real img is loaded
         console.log(imgElement.src);
         let given_im = cv.imread(imgElement);
-        
+
         //covert canvas to mat
-        let template = cv.imread(canvastopr);
+        //template right
+        let template = cv.imread(top_right);
+        console.log("Template rows and columns:",template.rows, template.cols);
+        //template left
+        //let templateLeft = cv.imread(top_left);
+        //template stat
+        //let templateStat = cv.imread(reff)
+        //console.log(template)
+        //console.log(template.rows, template.cols)
         let dst = new cv.Mat();
         let mask = new cv.Mat();
         cv.matchTemplate(given_im, template, dst, cv.TM_CCOEFF, mask);
-        let result = cv.minMaxLoc(dst, mask);
-        let maxPoint = result.maxLoc;
-        let color = new cv.Scalar(255, 0, 0, 255);
-        let point = new cv.Point(maxPoint.x + template.cols, maxPoint.y + template.rows);
-        cv.rectangle(given_im, maxPoint, point, color, 2, cv.LINE_8, 0);
+        //let result = cv.minMaxLoc(dst, mask);
+        //console.log("output of minMaxLoc",result);
+        //let maxPoint = result.maxLoc;
+        let minPoint = cv.minMaxLoc(dst, mask).minLoc;
+        console.log("Maxpoint",maxPoint,"Minpoint",minPoint);
+        //let color = new cv.Scalar(0, 255, 0, 255);
+        //let point = new cv.Point(minPoint.x+template.rows, minPoint.y+template.cols);
+        //console.log(point)
+        //let pointr = new cv.Point(maxPoint.x+template.rows, maxPoint.y+template.cols);
+        //console.log(pointr)
+        cv.rectangle(given_im, minPoint, new cv.Point(minPoint.x+template.rows, minPoint.y+template.cols), new cv.Scalar(0, 255, 0, 255), 2, cv.LINE_8, 0);
+        // so minPoint is the needed starting point, and 
+        //let point = new cv.Point(minPoint.x+template.rows, minPoint.y+template.cols);
+        // is the needed end point.
         cv.imshow('canvasOutput', given_im);
         given_im.delete();
         dst.delete();
