7edbc13c2a2425c0f51de23787c43826a5cfb72e
sagnik <sagnikdas53@live.com>
2020-12-07 00:30:37 +0530

    split the detector

--- node/detector_opencv.js
+++ node/detector_opencv.js
@@ -9,6 +9,27 @@ function getDateString() {
     return `${year}${month}${day}${hour}${minute}${second}`
 }
 
+function makebox(tem, rgb, r, g, b) {
+    try {
+        loadImageToCanvas(tem.src, document.getElementById("template_canvas"));
+        console.log("Processing ", tem, " Source ", tem.src);
+        let template = cv.imread(tem);
+        console.log("Template colums and rows:", template.cols, template.rows);
+        let dst = new cv.Mat();
+        let mask = new cv.Mat();
+        cv.matchTemplate(rgb, template, dst, cv.TM_CCOEFF, mask); //TM_CCOEFF -better  or TM_CCORR -shit
+        let minPoint = cv.minMaxLoc(dst, mask).minLoc;
+        let maxPoint = cv.minMaxLoc(dst, mask).maxLoc;
+        console.log("Maxpoint", maxPoint, "Minpoint", minPoint);
+        document.getElementById('x_cordd').innerHTML = "minpointX:maxpointX: " + minPoint.x+":"+maxPoint.x;
+        document.getElementById('y_cordd').innerHTML = "minpointY:maxpointY:" + minPoint.y+":"+maxPoint.y;
+        //rgb colour code is not working properly maybe colur space coverion is necessary
+        cv.rectangle(rgb, minPoint, new cv.Point(minPoint.x + template.cols, minPoint.y + template.rows), new cv.Scalar(0, r, 0, g, 0, b), 2, cv.LINE_8, 0); //works well
+        dst.delete();
+        mask.delete();
+    } catch (e) { console.error(e); }
+};
+
 function DownloadCanvasAsImage() {
     let downloadLink = document.createElement('a');
     downloadLink.setAttribute('download', 'Download_' + getDateString() + '.png');
@@ -57,29 +78,41 @@ window.onload = function () {
         main_image_ele.src = URL.createObjectURL(e.target.files[0]);
     }, false);
     main_image_ele.onload = function () {
-        loadImageToCanvas(main_image_ele.src,output_canavs);
+        let image_full = cv.imread(main_image_ele); //rgb one
+        let image = new cv.Mat();
+        // idk about the x and y coordinates but i assume rows=> y, cols=> x
+        const begin = Math.round(image_full.rows * 0.07)  //used only if black bar is present
+        console.log(begin);
+        //const begin = 00;
+        const y = Math.round(image_full.rows);
+        const x = Math.round(image_full.cols / 2);
+        console.log(x, y);  //used only if there i no black bar
+        let rect = new cv.Rect(begin, 00, x, y);
+        image = image_full.roi(rect);
 
+        //image = greyScale(image);   //not uses or tested
+
+        image_full.delete();
+        const r = 51;
+        const g = 219;
+        const b = 0;
         if (usage_stat_cb.checked === true) {
-            SAD(document.getElementById("output_canavs"), usage_stat_ele, "template_canvas");
-            return;
+            makebox(usage_stat_ele, image, r, g, b);
         }
         if (back_arrow_cb.checked === true) {
-            SAD(document.getElementById("output_canavs"), back_arrow_ele, "template_canvas");
-            return;
+            makebox(back_arrow_ele, image, r, g, b);          
         }
         if (attack_icon_cb.checked === true) {
-            SAD(document.getElementById("output_canavs"), attack_icon_ele, "template_canvas");
-            return;
+            makebox(attack_icon_ele, image, r, g, b);
         }
         if (critdam_icon_cb.checked === true) {
-            SAD(document.getElementById("output_canavs"), critdam_icon_ele, "template_canvas");
-            return;
+            makebox(critdam_icon_ele, image, r, g, b);
         }
         if (crossed_swo_cb.checked === true) {
-            SAD(document.getElementById("output_canavs"), crossed_swo_ele, "template_canvas");
-            return;
+            makebox(crossed_swo_ele, image, r, g, b);
         }
-        //SAD(document.getElementById("output_canavs"), crossed_swo_ele, "template_canvas");
+        cv.imshow('output_canavs', image);
+        image.delete();
     };
 }
 
@@ -94,49 +127,10 @@ function loadImageToCanvas(imageUrl, canvasElement) {
     };
 };
 
-function SAD(Src, Tem, id) {
-    console.log(Src, Tem, id);
-    let can = document.getElementById(id);
-    loadImageToCanvas(Tem.src, can);
-    let S_w = Src.width;
-    let S_h = Src.height;
-    let T_w = Tem.width;
-    let T_h = Tem.height;
-    let templateContext = can.getContext('2d');
-    let T = templateContext.getImageData(0, 0, T_w, T_h).data;
-    let originalContext = Src.getContext('2d');
-    let S = originalContext.getImageData(0, 0, S_w, S_h).data;
-    // this part is kept same the previous lines are patched in by me.
-    let minSAD = Number.MAX_SAFE_INTEGER;
-    let posX;
-    let posY;
-    for (let y = 0; y <= S_h - T_h; y++) {
-        for (let x = 0; x <= S_w - T_w; x++) {
-            let SAD = 0;
-            // loop through template image data.
-            for (let j = 0; j < T_h; j++) {
-                let S_idx = (y + j) * S_w;
-                let T_idx = j * T_w;
-                for (let i = 0; i < T_w; i++) {
-                    let S_idx_i = (S_idx + (x + i)) * 4;
-                    let T_idx_i = (T_idx + i) * 4;
-                    let S_pixel = S[S_idx_i];   // using R(ed) component.
-                    let T_pixel = T[T_idx_i];   // using R(ed) component.
-                    SAD += Math.abs(S_pixel - T_pixel);
-                }
-            }
-            if (minSAD > SAD) {
-                minSAD = SAD;
-                posX = x;
-                posY = y;
-            }
-        }
-    }
-    document.getElementById('x_cordd').innerHTML = "X: " + posX;
-    document.getElementById('y_cordd').innerHTML = "Y:" + posY;
-    originalContext.beginPath();
-    originalContext.rect(posX, posY, T_w, T_h);
-    originalContext.strokeStyle = 'red';
-    originalContext.stroke();
-}
 
+function greyScale(srccc) {
+    let dst = new cv.Mat();
+    // You can try more different parameters
+    cv.cvtColor(srccc, dst, cv.COLOR_RGBA2GRAY, 0);
+    return dst;
+}
