1fd5c8c97ddd053f67f0fa4e9a0ce51b50d1f19f
sagnik <sagnikdas53@live.com>
2020-12-06 20:14:30 +0530

    made all i could

--- node/detector.js
+++ node/detector.js
@@ -30,11 +30,11 @@ function makebox(tem, rgb, r, g, b) {
 
 function DownloadCanvasAsImage() {
     let downloadLink = document.createElement('a');
-    downloadLink.setAttribute('download', 'Download_' + getDateString() + '.png');
-    let canvas = document.getElementById("canvasOutput");
+    downloadLink.setattack_iconribute('download', 'Download_' + getDateString() + '.png');
+    let canvas = document.getElementById("output_canavs");
     canvas.toBlob(function (blob) {
         let url = URL.createObjectURL(blob);
-        downloadLink.setAttribute('href', url);
+        downloadLink.setattack_iconribute('hstats_box', url);
         downloadLink.click();
     });
 }
@@ -42,37 +42,50 @@ function DownloadCanvasAsImage() {
 
 window.onload = function () {
     //getting the images
-    let imgElement = document.getElementById('imageSrc');
-    let chbox1 = document.getElementById('TopLc');
-    console.log(chbox1.value);
-    let top_right = document.getElementById('Topr'); //the lightning shield one
-    let chbox2 = document.getElementById('TopRc');
-    console.log(chbox2.value);
-    let top_left = document.getElementById('Topl');
-    let reff = document.getElementById('ref');
-    let atta = document.getElementById('att');
-    let cd = document.getElementById('cdam');
-    let sw = document.getElementById('attSwo');
-    let top_left_big = document.getElementById('TopL');
+    let main_image_ele = document.getElementById('main_image');
+
+    let usage_stat_ele = document.getElementById('usage_stats'); //the lightning shield one
+    let usage_stat_cb = document.getElementById('usage_statsCB');
+    console.log(usage_stat_cb.checked);
+
+    let back_arrow_ele = document.getElementById('back_arrow');
+    let back_arrow_cb = document.getElementById('back_arrowCB');
+    console.log(back_arrow_cb.checked);
+
+    let stats_box_ele = document.getElementById('stats_box');
+    let stats_box_cb = document.getElementById('stats_boxCB');
+    console.log(stats_box_cb.checked);
+
+    let attack_icon_ele = document.getElementById('attack_icon');
+    let attack_icon_cb = document.getElementById('attack_iconCB');
+    console.log(attack_icon_cb.checked);
+
+    let critdam_icon_ele = document.getElementById('critdam_icon');
+    let critdam_icon_cb = document.getElementById('critdam_iconCB');
+    console.log(critdam_icon_cb.checked);
+
+    let crossed_swo_ele = document.getElementById('crossed_swo');
+    let crossed_swo_cb = document.getElementById('crossed_swoCB');
+    console.log(crossed_swo_cb.checked);
+
     let inputElement = document.getElementById('fileInput');
-    console.log("This are the stats" + reff.src);
-    console.log("Lightning shield " + top_right.src);
-    console.log("Back arrow " + top_left.src);
-    console.log("Attack " + atta.src);
-    console.log("Crit Damage " + cd.src);
-    console.log("Swords Locator " + sw.src);
-    console.log("Back arrow bigger " + top_left_big.src);
-    console.log(imgElement.src);
+    console.log("This are the stats" + stats_box_ele.src);
+    console.log("Lightning shield " + usage_stat_ele.src);
+    console.log("Back arrow " + back_arrow_ele.src);
+    console.log("attack_icon_eleck " + attack_icon_ele.src);
+    console.log("Crit Damage " + critdam_icon_ele.src);
+    console.log("Swords Locator " + crossed_swo_ele.src);
+    console.log(main_image_ele.src);
     //made the loader will reimplement once the detector works
     //this is used by setting the visibilty once tesseract starts working and stopping
     //stopping once it's done
     //var loader = document.getElementById("loader");
 
     inputElement.addEventListener('change', (e) => {
-        imgElement.src = URL.createObjectURL(e.target.files[0]);
+        main_image_ele.src = URL.createObjectURL(e.target.files[0]);
     }, false);
-    imgElement.onload = function () {
-        let image_full = cv.imread(imgElement); //rgb one
+    main_image_ele.onload = function () {
+        let image_full = cv.imread(main_image_ele); //rgb one
         let image = new cv.Mat();
         // idk about the x and y coordinates but i assume rows=> y, cols=> x
         //const begin = Math.round(image_full.rows*0.07)  used only if black bar is present
@@ -83,12 +96,28 @@ window.onload = function () {
         //console.log(x, y);
         let rect = new cv.Rect(begin, 00, x, y);
         image = image_full.roi(rect);
-        cv.imshow('canvasOutput', image);
+        cv.imshow('output_canavs', image);
         image.delete();
         image_full.delete();
-        // SAD template matching test
-        //SAD(document.getElementById("canvasOutput"),top_right,"tempc");
-        SAD(document.getElementById("canvasOutput"),top_left_big,"tempc");
+
+        if (usage_stat_cb.checked === true) {
+            SAD(document.getElementById("output_canavs"), usage_stat_ele, "template_canvas");
+        }
+        if (back_arrow_cb.checked === true) {
+            SAD(document.getElementById("output_canavs"), back_arrow_ele, "template_canvas");
+        }
+        if (stats_box_cb.checked === true) {
+            SAD(document.getElementById("output_canavs"), stats_box_ele, "template_canvas");
+        }
+        if (attack_icon_cb.checked === true) {
+            SAD(document.getElementById("output_canavs"), attack_icon_ele, "template_canvas");
+        }
+        if (critdam_icon_cb.checked === true) {
+            SAD(document.getElementById("output_canavs"), critdam_icon_ele, "template_canvas");
+        }
+        if (crossed_swo_cb.checked === true) {
+            SAD(document.getElementById("output_canavs"), crossed_swo_ele, "template_canvas");
+        }
     };
 }
 
@@ -112,7 +141,7 @@ function SAD(Src, Tem, id) {
     let T_w = Tem.width;
     let T_h = Tem.height;
     //console.log(S_w, S_h, T_w, T_h);
-    let templateContext = can.getContext('2d'); 
+    let templateContext = can.getContext('2d');
     let T = templateContext.getImageData(0, 0, T_w, T_h).data;
     let originalContext = Src.getContext('2d');
     let S = originalContext.getImageData(0, 0, S_w, S_h).data;
@@ -142,8 +171,8 @@ function SAD(Src, Tem, id) {
             }
         }
     }
-    document.getElementById('x_cordd').innerHTML = "X: "+posX;
-    document.getElementById('y_cordd').innerHTML = "Y:"+posY;
+    document.getElementById('x_cordd').innerHTML = "X: " + posX;
+    document.getElementById('y_cordd').innerHTML = "Y:" + posY;
     originalContext.beginPath();
     originalContext.rect(posX, posY, T_w, T_h);
     originalContext.strokeStyle = 'red';
