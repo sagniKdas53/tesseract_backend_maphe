23d1705a2debf17c8ffde3464fb34fbf5a59e768
Sagnik Das <sagnikdas53@live.com>
2021-07-14 20:43:13 +0530

    updated the detctor

--- tests/detector.py
+++ tests/detector.py
@@ -3,14 +3,14 @@ import os
 import getopt
 import cv2 as cv
 import numpy as np
-
+from icecream import ic
 
 def crop(out, loc_l, loc_r, hi, img_rgb):
     x_s = np.amax(loc_r[0])
     xs = np.amin(loc_l[0])
     ys = np.amin(loc_r[1])  # also use as x end
     ye = hi
-    print(x_s, ":", ye, ',', xs, ":", x_s,)
+    ic(x_s, ":", ye, ',', xs, ":", x_s,)
     crop_img = img_rgb[x_s:ye, xs:ys]  # add the dimensions later
     #cv.imshow("Final Crop",crop_img)
     # cv.waitKey(0)
@@ -56,16 +56,18 @@ def make_rect(out, loc_r, img_rgb, wr, hr, hi, loc_l, wl, hl):
 
 
 '''def verbose():
-    print('\n', loc_l, '\n\n', loc_r)
-    print(loc_l[0][0], '\n', loc_r[0][0], '\n', hi, '\n', hi)'''
+    ic('\n', loc_l, '\n\n', loc_r)
+    ic(loc_l[0][0], '\n', loc_r[0][0], '\n', hi, '\n', hi)'''
 
 
 def locate_data(out, threshold, templateTomatch):
-    print(templateTomatch, '\n', out+'_cropped.png')
+    cwd = os.getcwd()
+    ic(templateTomatch, '\n', out+'_cropped.png')
     tempF = cv.imread(templateTomatch, 0)
     # out+'_cropped.png')
-    det_im = cv.imread(
-        '/home/sagnik/Projects/tesseract_backend_maphe/tests/outfile_cropped.png')
+    ic(cwd+out+"_cropped.png")
+    det_im = cv.imread(cwd+'/'+out+"_cropped.png")
+    # '/home/sagnik/Projects/tesseract_backend_maphe/tests/outfile_cropped.png')
     img_gr = cv.cvtColor(det_im, cv.COLOR_BGR2GRAY)
     res = cv.matchTemplate(img_gr, tempF, cv.TM_CCOEFF_NORMED)
     '''cv.imshow("image", img_gr)
@@ -76,21 +78,21 @@ def locate_data(out, threshold, templateTomatch):
     cv.waitKey(0)'''
     try:
         for i in range(0, 10):
-            print("threshold = ", i/10)
+            ic("threshold = ", i/10)
             loc = np.where(res >= i/10)
             w, h = tempF.shape[::-1]
             x_m = np.amax(loc[0])
             y_m = np.amax(loc[1])
     except ValueError:
-        i-=1
-        print("threshold too high, trying a last accepatble value=",i/10)
+        i -= 1
+        ic("threshold too high, trying a last accepatble value=", i/10)
         loc = np.where(res >= i/10)
         w, h = tempF.shape[::-1]
         x_m = np.amax(loc[0])
         y_m = np.amax(loc[1])
     #x_n = np.amin(loc[0])
     #ye = np.amin(loc[1])
-    #print(x_m, y_m, x_n, ye,h,w)
+    #ic(x_m, y_m, x_n, ye,h,w)
     crop_img = det_im[x_m:x_m+h, y_m:y_m+w]
     #cv.imshow("cropped", crop_img)
     # cv.waitKey(0)
@@ -106,54 +108,64 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv, "hi:o:", ["if=", "of="])
     except getopt.GetoptError:
-        print('detector.py -i <inputfile> -o <outputfile>')
+        ic('detector.py -i <inputfile> -o <outputfile>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('detector.py -i <inputfile> -o <outputfile>')
+            ic('detector.py -i <inputfile> -o <outputfile>')
             sys.exit()
         elif opt in ("-i", "--if"):
             inputfile = arg
         elif opt in ("-o", "--of"):
             outputfile = arg
-    print(inputfile, outputfile)
+            outputfile = outputfile+'-'+inputfile
+    ic(inputfile, outputfile)
     file = inputfile
     original_im = cv.imread(file, 0)
     img_rgb = cv.imread(file)
-    print(img_rgb.shape)
+    ic(img_rgb.shape)
     img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
-    print(cwd+'/templates/')
+    ic(cwd+'/templates/')
     templatel = cv.imread(cwd+'/templates/top_left.jpg', 0)
     templater_active = cv.imread(
         cwd+'/templates/top_right_swords_active.jpg', 0)
     templater_in_active = cv.imread(
         cwd+'/templates/top_right_swords_inactive.jpg', 0)
+    stat_template = cv.imread(cwd+'/templates/top_right.jpg', 0)
     template = cv.imread(cwd+'/templates/necessary_col.jpg', 0)
     template_to_match = cwd+'/templates/necessary_col.jpg'
-    print(template, '\n', templatel, '\n',
+    ic(template, '\n', templatel, '\n',
           templater_in_active, '\n', templater_active)
     wi, hi = original_im.shape[::-1]
     wl, hl = templatel.shape[::-1]
     wr, hr = templater_active.shape[::-1]
     resl = cv.matchTemplate(img_gray, templatel, cv.TM_CCOEFF_NORMED)
-    resr = cv.matchTemplate(img_gray, templater_active, cv.TM_CCOEFF_NORMED)
-    threshold = 0.8
+    #resr = cv.matchTemplate(img_gray, templater_active, cv.TM_CCOEFF_NORMED)
+    resr = cv.matchTemplate(img_gray, stat_template, cv.TM_CCOEFF_NORMED)
+    threshold = 0.8  # tested and found
     loc_l = np.where(resl >= threshold)
     loc_r = np.where(resr >= threshold)
     try:
-        print('\nloc_l:', loc_l, '\n\nloc_r:', loc_r)
-        print(loc_l[0][0], '\n', loc_r[0][0], '\n', hi, '\n', hi)
+        ic('\nloc_l:', loc_l, '\n\nloc_r:', loc_r)
+        ic(loc_l[0][0], '\n', loc_r[0][0], '\n', hi, '\n', hi)
         #templater = templater_active
     except IndexError:
-        wr, hr = templater_in_active.shape[::-1]
-        resr = cv.matchTemplate(
-            img_gray, templater_in_active, cv.TM_CCOEFF_NORMED)
-        loc_r = np.where(resr >= threshold)
-        print(loc_l[0][0], '\n', loc_r[0][0], '\n', hi, '\n', hi)
+        try:
+            wr, hr = templater_in_active.shape[::-1]
+            resr = cv.matchTemplate(
+                img_gray, templater_in_active, cv.TM_CCOEFF_NORMED)
+            loc_r = np.where(resr >= threshold)
+            ic(loc_l[0][0], '\n', loc_r[0][0], '\n', hi, '\n', hi)
+        except IndexError:
+            wr, hr = templater_active.shape[::-1]
+            resr = cv.matchTemplate(
+                img_gray, templater_active, cv.TM_CCOEFF_NORMED)
+            loc_r = np.where(resr >= threshold)
+            ic(loc_l[0][0], '\n', loc_r[0][0], '\n', hi, '\n', hi)
         #templater = templater_in_active
-    point_pixels(outputfile,img_rgb,loc_r,loc_l,wr,hl)
-    make_rect(outputfile,loc_r,img_rgb,wr,hr,hi,loc_l,wl,hl)
-    crop(outputfile,loc_l,loc_r,hi,img_rgb)
+    point_pixels(outputfile, img_rgb, loc_r, loc_l, wr, hl)
+    make_rect(outputfile, loc_r, img_rgb, wr, hr, hi, loc_l, wl, hl)
+    crop(outputfile, loc_l, loc_r, hi, img_rgb)
     locate_data(outputfile, threshold, template_to_match)
 
 
